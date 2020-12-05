83f147bc681ac0bad14b9f32d790d5e6717cad3a
Diego Imbert <imbert.diego05000@gmail.com>
2020-12-05 20:20:45 +0100

    Recreation des le depart (oui...)

--- src/Automata.py
+++ src/Automata.py
@@ -1,91 +1,40 @@
+from itertools import *
+
 class Automata:
     class State:
-        def __init__(self, name):
+        def __init__(self, name : str, accepting : bool = False):
             self.name = name
-            self.accepting = False
-        def toString(self):
-            return self.name + (" (Accepting)" if self.accepting else "")
-    class Transition:
-        def __init__(self, sourceState, destState, symbol):
-            self.sourceState = sourceState # String
-            self.destState = destState     # String
-            self.symbol = symbol           # String
-        def toString(self):
-            return self.sourceState + " --" + self.symbol + "-> " + self.destState
+            self.accepting = accepting
+            self.transitions = [] # Liste de tuples (symbol : str, destination : State)
+        def __str__(self):
+            return self.name + '*'*self.accepting
+    
+    # transitions est un tuple de 3 (origin, symbol, destination)
+    # Dans tout le reste du code, c'est un tuple de 2 (car origin est deduit de l'etat contenant)
+    def __init__(self, transitions, acceptingStates : [str]):
+        def getTransitions(state):
+            for origin, symbol, destination in transitions:
+                if origin == state.name:  
+                    yield((symbol, destination))
 
-    def __init__(self, states, transitions):
-        self.states = states            # [State]
-        self.transitions = transitions  # [Transition]
+        # We parse all the different states
+        statesStrings = set(chain(*map(lambda t: [t[0], t[2]], transitions)))
 
-    def getInitialState(self):
-        return self.states[0]
-    def getStateByName(self, name):
-        return list(filter(lambda s: s.name == name, self.states))[0]
-    # Returns all transitions which starting state is 'state'
-    def getTransitions(self, state):
-        return list(filter(lambda t: t.sourceState == state.name, self.transitions))
-    def getAlphabet(self):
-        alphabet = []
-        for transition in self.transitions:
-            if alphabet.count(transition.symbol) == 0:
-                alphabet.append(transition.symbol)
-        return alphabet
-
-
-    def isDeterministic(self):
+        # We convert the parsed states to State objects
+        self.states = list(map(lambda s: Automata.State(s, s in acceptingStates), statesStrings))
+        
+        # On ajoute chaque transition à son etat d'origine
         for state in self.states:
-            # On fait une liste de toutes les transitions partant de l'etat state
-            outTransitions = self.getTransitions(state)
-            outSymbols = list(map(lambda t: t.symbol, outTransitions))
+            state.transitions.extend(getTransitions(state))
+    @staticmethod
+    def from_string(string : str):
+        transitions = [ tuple(line.split()) for line in string.split('\n')[:-1] ]
+        acceptingStates = string.split('\n')[-1][2:]
+        return Automata(transitions, acceptingStates)
 
-            # On verifie qu'il n'y ait pas d'epsilon transition autre que la transition sur soi meme
-            if list(filter(lambda t: t.symbol == '%' and t.sourceState != t.destState, outTransitions)) != []:
-                return False
-
-            # On verifie qu'il n'y a pas de duplicats
-            if len(set(outSymbols)) != len(outSymbols):
-                return False
-        return True
-    def isAccepted(self, str):
-        currentState = self.getInitialState()
-        for char in str:
-            if (char == '%'):
-                continue
-            # On fait une liste des transitions possibles depuis currentState avec le symbole char.
-            # Si l'automate est deterministe cette liste contient soit 0 soit 1 elements
-            transitions = list(filter(lambda t: t.symbol == char, self.getTransitions(currentState)))
-            # Si l'etat n'a pas de transition pour la lettre alors le mot n'est pas reconnu
-            if (len(transitions) == 0):
-                return False
-            # Sinon on met a jour currentState.
-            # Notons qu'on prend simplement le premier element de transitions,
-            # car on suppose que l'automate est deterministe.
-            # Sinon il faudrait implementer un algorithme recursif.
-            currentState = list(filter(lambda s: s.name == transitions[0].destState, self.states))[0]
-        return currentState.accepting
-
-def textToAutomata(sourceTxt):
-    states = []
-    transitions = []
-    lines = sourceTxt.split('\n')
-    # We parse each line defining a transition
-    # le [:-2] retire la derniere ligne (vide) et la ligne definissant les transitions
-    for words in map(lambda l: l.split(' '), lines[:-1]):
-        # We parse the content of the line
-        sourceState = words[0]
-        destState   = words[2]
-        symbol      = words[1]
-        # We add the two states if they're not already in the list
-        if list(map(lambda s: s.name, states)).count(sourceState) == 0:
-            states.append(Automata.State(sourceState))
-        if list(map(lambda s: s.name, states)).count(destState) == 0:
-            states.append(Automata.State(destState))
-        # We add the transition in the list
-        transitions.append(Automata.Transition(sourceState, destState, symbol))
-    # We parse the last line i.e the accepting states
-    acceptingStates = lines[-1].split(' ')[+1:]
-    for state in states:
-        for accepting in acceptingStates:
-            if state.name == accepting:
-                state.accepting = True
-    return Automata(states, transitions)
+    ######################################
+    #                                    #
+    #                TP 1                #
+    #                                    #
+    ######################################
+    
---
+++ src/Main.py
@@ -0,0 +1,17 @@
+from Automata import *
+import sys
+import os
+
+# On verifie les erreurs evidentes de l'utilisateur
+def print_and_exit(str): print(str); sys.exit(-1)
+if (len(sys.argv) != 3): print_and_exit('Usage: ./automata \'path_to_file.af\' \'word_to_find\'')
+if not os.path.exists(sys.argv[1]): print_and_exit("Error: file does not exist")
+if not os.path.isfile(sys.argv[1]): print_and_exit("Error: first argument must be a path to a file")
+
+# On lit le fichier et le mot à trouver
+with open(sys.argv[1], 'r') as file: source = file.read()
+word = sys.argv[2]
+
+#On crée l'automate a partir du fichier
+try: automata = Automata.from_string(source)
+except ValueError: print_and_exit('Invalid input file')
