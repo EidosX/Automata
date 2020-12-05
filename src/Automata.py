fdd7c2c923ba0090a12e44c2d06244d1c535effe
Diego Imbert <imbert.diego05000@gmail.com>
2020-12-05 21:15:10 +0100

    TP 1

--- src/Automata.py
+++ src/Automata.py
@@ -7,18 +7,28 @@ class Automata:
             self.accepting = accepting
             self.transitions = [] # Liste de tuples (symbol : str, destination : State)
         def __str__(self):
-            return self.name + '*'*self.accepting
+            return self.name + '*' * self.accepting
+        def getNextState(self, symbol : str):
+            # Devrait tout le temps etre de longueur 1 ou 0, car l'automate est deterministe
+            matchingTransitions = list(filter(lambda t: t[0] == symbol, self.transitions))
+            for t in matchingTransitions: 
+                return t[1]
     
     # transitions est un tuple de 3 (origin, symbol, destination)
     # Dans tout le reste du code, c'est un tuple de 2 (car origin est deduit de l'etat contenant)
-    def __init__(self, transitions, acceptingStates : [str]):
+    def __init__(self, transitions : tuple, initialState : str, acceptingStates : [str]):
         def getTransitions(state):
             for origin, symbol, destination in transitions:
-                if origin == state.name:  
-                    yield((symbol, destination))
+                if origin == state.name: yield((symbol, self.getStateByName(destination)))
+
+        self.states = None
+        self.initialState = None
+
+        # On enleve les transitions epsilon inutiles d'un etat à ce meme etat
+        transitions = list(filter(lambda t: not (t[1] == '%' and t[0] == t[2]), transitions))
 
         # We parse all the different states
-        statesStrings = set(chain(*map(lambda t: [t[0], t[2]], transitions)))
+        statesStrings = chain(*map(lambda t: [t[0], t[2]], transitions))
 
         # We convert the parsed states to State objects
         self.states = list(map(lambda s: Automata.State(s, s in acceptingStates), statesStrings))
@@ -26,15 +36,35 @@ class Automata:
         # On ajoute chaque transition à son etat d'origine
         for state in self.states:
             state.transitions.extend(getTransitions(state))
+            if state.name == initialState: self.initialState = state
+
+        if not self.isDeterministic(): print('WARNING: Not Deterministic')
     @staticmethod
     def from_string(string : str):
         transitions = [ tuple(line.split()) for line in string.split('\n')[:-1] ]
         acceptingStates = string.split('\n')[-1][2:]
-        return Automata(transitions, acceptingStates)
+        return Automata(transitions, transitions[0][0], acceptingStates)
+    
+    def getStateByName(self, state_name : str):
+        for state in self.states:
+            if state.name == state_name: return state
 
     ######################################
     #                                    #
-    #                TP 1                #
+    #            --- TP 1 ---            #
     #                                    #
     ######################################
     
+    def isDeterministic(self):
+        # PS: On ne verifie pas quand le symbole est epsilon si les deux etats sont bien differents
+        #     car toutes ces transitions (X vers X via epsilon) ont été supprimées dans __init__
+        for state in self.states:
+            for key, l in groupby(sorted(map(lambda t: t[0], state.transitions))):
+                if (key == '%' or len(list(l)) > 1): return False
+        return True
+    def isRecognized(self, word : str):
+        state = self.initialState
+        for symbol in word:
+            if state != None: state = state.getNextState(symbol)
+            else: break
+        return state != None and state.accepting
